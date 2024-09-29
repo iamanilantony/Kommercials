149b6f9592ee29369868b23ef4c0c502139a737f
iamanilantony <anilantony3134@gmail.com>
2024-09-29 08:04:41 +0530

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Kommercials
+# PortFolio-V3
+After doing 5 different Iterations and 17 different Designs this my current porfolio (Atleast for now)😁
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.astro", // Include Astro components
+    "./src/**/*.js" // Include JavaScript files
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
